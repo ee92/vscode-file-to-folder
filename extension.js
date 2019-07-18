8d476e3fe3b96e14cc7bab3a62d4395fa8a3a817
egor <eegor650@gmail.com>
2019-07-18 15:21:46 -0700

    handle multi-line imports, .jsx, .ts, .tsx

--- extension.js
+++ extension.js
@@ -1,44 +1,55 @@
 const vscode = require('vscode');
 const fs = require('fs');
 
+const importRegex = /(import[^'"]*|require\()('(.*?)'|"(.*?)")/g
+const stringRegex = /('(.*?)'|"(.*?)")/
+
 const formatImportExport = (name) => (
-	`import ${name} from './${name}';
-	
-	export default ${name}`
+`import ${name} from './${name}';
+
+export default ${name};`
 )
+
+const updatePath = (s) => {
+	const prefix = s.slice(1,3);
+	switch(prefix) {
+		case('./'):
+			return s.substr(0,1) + '.' + s.substr(1);
+		case('..'):
+			return s.substr(0,1) + '../' + s.substr(1);
+		default:
+			return s;
+	}
+}
 
 const updateLocalImports = (path) => {
 	const text = fs.readFileSync(path, 'utf-8');
-	const updatedText = text.replace(/(from '\.|from "\.).*/g, (match) => {
-		const str = match.replace(/('\.|"\.).*('|")/g, (s) => {
-			const prefix = s.slice(1,3);
-			if (prefix === './') {
-				return s.substr(0,1) + '.' + s.substr(1);
-			}
-			else if (prefix === '..') {
-				return s.substr(0,1) + '../' + s.substr(1);
-			}
-			return s;
-		})
-		return str;
+	const updatedText = text.replace(importRegex, (match) => {
+		const newPath = match.replace(stringRegex, updatePath)
+		return newPath;
 	})
 	fs.writeFileSync(path, updatedText);
+}
+
+const createReactComponentFolder = (event) => {
+	const path = event.fsPath;
+	const pathExt = path.split('.').pop();
+	const pathNoExt = path.split('.').slice(0,-1).join('.');
+	const fileName = pathNoExt.split('/').pop();
+	const newPath = pathNoExt + '/' + fileName + '.' + pathExt;
+	const indexPath = pathNoExt + '/index.' + pathExt;
+	const indexText = formatImportExport(fileName);
+
+	fs.mkdirSync(pathNoExt);
+	fs.writeFileSync(indexPath, indexText);
+	fs.renameSync(path, newPath);
+	updateLocalImports(newPath);
 }
 
 const activate = (context) => {
 	let disposable = vscode.commands.registerCommand(
 		'extension.createReactComponentFolder',
-		(event) => {
-			const path = event.fsPath;
-			const pathNoExt = path.split('.').slice(0,-1).join('.');
-			const fileName = pathNoExt.split('/').pop();
-
-			fs.mkdirSync(pathNoExt);
-			fs.writeFileSync(pathNoExt + '/' + fileName + '.module.css', '');
-			fs.writeFileSync(pathNoExt + '/index.js', formatImportExport(fileName));
-			fs.renameSync(path, pathNoExt + '/' + fileName + '.js');
-			updateLocalImports(pathNoExt + '/' + fileName + '.js');
-		}
+		createReactComponentFolder
 	);
 	context.subscriptions.push(disposable);
 }
