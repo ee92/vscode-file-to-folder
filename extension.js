7c06a4826d5b65a940a7ce4ed9a0ea507df5c3ab
egor <eegor650@gmail.com>
2019-07-19 15:17:07 -0700

    add options to generate additional files

--- extension.js
+++ extension.js
@@ -1,14 +1,21 @@
 const vscode = require('vscode');
 const fs = require('fs');
 
-const importRegex = /(import[^'"]*|require\()('(.*?)'|"(.*?)")/g
-const stringRegex = /('(.*?)'|"(.*?)")/
+const importRegex = /(import[^'"]*|require\()('(.*?)'|"(.*?)")/g;
+const stringRegex = /('(.*?)'|"(.*?)")/;
 
-const formatImportExport = (name) => (
-`import ${name} from './${name}';
-
-export default ${name};`
-)
+const extraOptions = (context) => [
+	{	
+		'id': 'test',
+		'label': 'generate test file',
+		'description': `${context.fileName}.test.${context.pathExt}`
+	},
+	{
+		'id': 'css_module',
+		'label': 'generate css module',
+		'description': `${context.fileName}.module.css`
+	}
+]
 
 const updatePath = (s) => {
 	const prefix = s.slice(1,3);
@@ -31,25 +38,62 @@ const updateLocalImports = (path) => {
 	fs.writeFileSync(path, updatedText);
 }
 
-const createReactComponentFolder = (event) => {
+const createFolder = (context, options) => {
+	fs.mkdirSync(context.folderPath);
+	fs.writeFileSync(context.indexPath, context.indexText);
+	fs.renameSync(context.path, context.newPath);
+	if (options) {
+		options.forEach((option) => {
+			const name = option.description;
+			const path = context.folderPath + '/' + name;
+			fs.writeFileSync(path, '');
+		})
+	}
+	updateLocalImports(context.newPath);
+}
+
+const showOptions = (context) => {
+	return vscode.window.showQuickPick(
+		extraOptions(context),
+		{
+			placeHolder: 'generate additional files',
+			canPickMany: true,
+			ignoreFocusOut: false
+		}
+	)
+}
+
+const formatImportExport = (name) => (
+	`import ${name} from './${name}';\n\nexport default ${name};`
+)
+
+const init = (event) => {
 	const path = event.fsPath;
 	const pathExt = path.split('.').pop();
-	const pathNoExt = path.split('.').slice(0,-1).join('.');
-	const fileName = pathNoExt.split('/').pop();
-	const newPath = pathNoExt + '/' + fileName + '.' + pathExt;
-	const indexPath = pathNoExt + '/index.' + pathExt;
+	const folderPath = path.split('.').slice(0,-1).join('.');
+	const fileName = folderPath.split('/').pop();
+	const newPath = folderPath + '/' + fileName + '.' + pathExt;
+	const indexPath = folderPath + '/index.' + pathExt;
 	const indexText = formatImportExport(fileName);
 
-	fs.mkdirSync(pathNoExt);
-	fs.writeFileSync(indexPath, indexText);
-	fs.renameSync(path, newPath);
-	updateLocalImports(newPath);
+	const context = {
+		path,
+		pathExt,
+		folderPath,
+		fileName,
+		newPath,
+		indexPath,
+		indexText
+	}
+	
+	showOptions(context)
+	.then((selectedOptions) => createFolder(context, selectedOptions))
 }
 
 const activate = (context) => {
 	let disposable = vscode.commands.registerCommand(
-		'extension.createReactComponentFolder',
-		createReactComponentFolder
+		'extension.folderize',
+		init
 	);
 	context.subscriptions.push(disposable);
 }
